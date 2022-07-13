0f0507bf9f1f860cc2d393ead2cce654687f9514
Marllon Panisset <marllonop@gmail.com>
2022-07-13 19:02:04 -0300

    estudando react styled components

---
+++ app/src/App.js
@@ -0,0 +1,21 @@
+import {Header, Main, Footer} from './styles';
+
+function App() {
+  return (
+    <div>
+      <Header>
+        <h1>HEADER</h1>
+      </Header>
+
+      <Main>
+        <h3>Main</h3>
+      </Main>
+
+      <Footer>
+        <p>Footer</p>
+      </Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+    background: #000;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        color: #fff
+    }
+`;
+
+export const Main = styled.main`
+    margin-top: 50px;
+    min-height: 500px;
+    background: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h3 {
+        color: #fff
+    }
+`;
+
+export const Footer = styled.footer`
+    margin-top: 50px;
+    height: 100px;
+    background: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    p {
+        color: #fff
+    }
+`;
